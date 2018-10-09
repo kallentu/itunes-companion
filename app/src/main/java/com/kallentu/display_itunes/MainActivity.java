38a230e40beedc4753be3793ebdefe6825e30377
kallentu <kallentu@hotmail.ca>
2018-10-09 18:32:05 -0400

    Initial.

---
+++ app/src/main/java/com/kallentu/display_itunes/MainActivity.java
@@ -0,0 +1,18 @@
+package com.kallentu.display_itunes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // TODO (1): Take input fron the EditText
+    // TODO (2): Use input to search for a response
+    // TODO (3): Make a class that stores the responses from iTunes
+    // TODO (4): Make a list adapter that stores each result
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
