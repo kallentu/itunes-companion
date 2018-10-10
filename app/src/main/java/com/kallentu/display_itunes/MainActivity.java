548747268c2311bed991f54da0690cf6d3c6d19c
kallentu <kallentu@hotmail.ca>
2018-10-10 11:05:11 -0400

    Moved iTunesResultsTask to be a subclass of MainActivity, added iTunesResultsListAdapter to display result items.

--- app/src/main/java/com/kallentu/display_itunes/MainActivity.java
+++ app/src/main/java/com/kallentu/display_itunes/MainActivity.java
@@ -1,16 +1,28 @@
 package com.kallentu.display_itunes;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import be.ceau.itunesapi.Search;
+import be.ceau.itunesapi.response.Response;
+import be.ceau.itunesapi.response.Result;
+
+// TODO (6): Add tests for the adapter
+// TODO (7): Stylize the adapter contents/xml
+// TODO (8): Add album art if available
 
 /** Primary activity for obtaining user query and searching. */
 public class MainActivity extends AppCompatActivity {
     private EditText searchBar;
-
-    // TODO (4): Make a list adapter that stores each result
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +44,36 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /** Runs async task {@link iTunesResultsTask} */
+    /** Runs async task {@link iTunesResultsTask} to get iTunes API results from search query. */
     private void getiTunesResponse() {
         String query = searchBar.getText().toString().replace(' ', '+');
         new iTunesResultsTask().execute(query);
     }
+
+    /** Asynchronous task for obtaining iTunes API results for {@link MainActivity} */
+    @SuppressLint("StaticFieldLeak")
+    class iTunesResultsTask extends AsyncTask<String, Integer, List<Result>> {
+
+        @Override
+        protected List<Result> doInBackground(String... strings) {
+            List<Result> results = new ArrayList<>();
+            for (String query : strings) {
+                Response response = new Search(query).execute();
+                results.addAll(response.getResults());
+            }
+            return results;
+        }
+
+        @Override
+        protected void onPostExecute(List<Result> results) {
+            super.onPostExecute(results);
+
+            // Sets the list of results on main page
+            iTunesResultsListAdapter adapter = new iTunesResultsListAdapter(getApplicationContext(), R.layout.adapter_result, results);
+            ListView resultsList = (ListView)findViewById(R.id.results_list);
+            resultsList.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/kallentu/display_itunes/iTunesResultsListAdapter.java
@@ -0,0 +1,63 @@
+package com.kallentu.display_itunes;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import be.ceau.itunesapi.response.Result;
+
+/**
+ * Array adapter for displaying the API results from {@link MainActivity}.
+ */
+public class iTunesResultsListAdapter extends ArrayAdapter<Result> {
+    private final List<Result> results;
+
+    public iTunesResultsListAdapter(@NonNull Context context, int resource, List<Result> results) {
+        super(context, resource);
+        this.results = results;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Result result = results.get(position);
+
+        // Checks for existing view already used
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.adapter_result, parent, false);
+        }
+
+        TextView resultSongTitle = (TextView) convertView.findViewById(R.id.result_song_title);
+        TextView resultArtist = (TextView) convertView.findViewById(R.id.result_artist);
+        TextView resultAlbum = (TextView) convertView.findViewById(R.id.result_album);
+
+        resultSongTitle.setText(result.getArtistName());
+        resultArtist.setText(result.getTrackName());
+        resultAlbum.setText(result.getCollectionName());
+
+        return convertView;
+    }
+
+    @Nullable
+    @Override
+    public Result getItem(int position) {
+        return results.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getCount() {
+        return results.size();
+    }
+}
